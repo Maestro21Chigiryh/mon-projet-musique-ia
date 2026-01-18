301ca4b3ca0b8457e56c479ce4b2b06e701a8021
FaniryNomeaRomeo <fanirinomenaromeo@gmail.com>
2026-01-18 10:43:36 +0300

    Connexion React-FastAPI et ajout de Tone.js pour le son

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware # Importation de la sécurité
 
 app = FastAPI()
 
+# Configuration du CORS : c'est comme donner une clé à React
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # On autorise tout le monde pour le moment
+    allow_methods=["*"], # On autorise tous les types de requêtes (GET, POST...)
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def read_root():
-    return {"message": "Bienvenue sur l'API de musique IA"}
+    return {"message": "Salut ! L'IA est prête à t'écouter."}
